6dee45c2d3c204f557421fa1048333bc0a6f103e
GlebZyryanov <darrenshen02@gmail.com>
2024-04-20 13:30:47 +0300

    создание класса Converter и функций в нем

---
+++ Converter.py
@@ -0,0 +1,12 @@
+# класс конвертер
+class Converter:
+    @staticmethod
+    def meters_to_feet(meters):
+        feet = meters * 3.28084
+        return feet
+
+    @staticmethod
+    def kilograms_to_pounds(kilograms):
+        pounds = kilograms * 2.20462
+        return pounds
+
