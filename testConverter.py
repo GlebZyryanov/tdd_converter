909f6199b456d85c00b689549868697e6c2c498c
GlebZyryanov <darrenshen02@gmail.com>
2024-04-20 13:27:03 +0300

    создание теста на наличие класса Convert

--- testConverter.py
+++ testConverter.py
@@ -1,11 +1,13 @@
 # файл для тестирования класса конвертер
+
 import unittest
 
 
 class TestConverterClass(unittest.TestCase):
+
     def test_converter_class_exists(self):
-        self.assertTrue(hasattr(Converter, '__init__'), "Converter class should have '__init__' method")
-        self.assertTrue(hasattr(Converter, 'convert'), "Converter class should have 'convert' method")
+        self.assertTrue(hasattr(Converter, 'meters_to_feet'))
+        self.assertTrue(hasattr(Converter, 'kilograms_to_pounds'))
 
 if __name__ == '__main__':
     unittest.main()
